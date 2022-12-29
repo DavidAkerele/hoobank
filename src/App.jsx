a31f7a63487e1c7e75ea4926adee889eeba0a64a
DavidIsCool <104978550+DavidAkerele@users.noreply.github.com>
2022-12-29 17:23:40 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => (
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX}${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
